bb6aaaa52fada7d87e368f3e91415c6e4f8fd4e1
Julian- <Julian-@Julian>
2017-02-22 05:14:52 -0500

    Repaso 2

---
+++ src/repaso/DrumKit.java
@@ -0,0 +1,18 @@
+package repaso;
+
+
+public class DrumKit {
+    
+    boolean topHat = true;
+    boolean snare = true;
+    
+    void playSnare(){
+        System.out.println("bang bang ba-bang");
+    }
+    void playTopHat(){
+        System.out.println("ding ding da-ding");
+    }
+    
+}
+    
+
